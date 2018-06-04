d4741df12541defffa02242ff0afd5c53d8126d8
Armando Gonzalez <agonzalez9@ggc.edu>
2018-06-04 12:26:28 -0400

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/healthyrecipes/Recipe.java
@@ -0,0 +1,19 @@
+package net.androidbootcamp.healthyrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Recipe extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+
+
+}
